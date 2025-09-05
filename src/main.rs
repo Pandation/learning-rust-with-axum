6e7f6b5bff5af4d6bfb6e2893bbea731193497ce
Florian Baumes <florianbaumes@gmail.com>
2025-09-06 01:00:52 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+
+    axum::serve(TcpListener::bind(addr).await.unwrap(), app).await?;
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    "Hello, world!"
+}
